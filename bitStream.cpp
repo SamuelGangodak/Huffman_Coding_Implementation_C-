641dcd3a11975f4dc8830b0a1b3b1066b1488428
SamuelGangodak <samuelgangodak79@gmail.com>
2024-07-26 07:14:06 +0530

    modified bitStream.cpp

--- bitStream.cpp
+++ bitStream.cpp
@@ -3,7 +3,7 @@ using namespace::std;
 
 class BitStream{
     private:
-        unsigned char byte;
+        char byte;
         int size;
     public:
         BitStream() {
@@ -12,11 +12,13 @@ class BitStream{
         }
         bool is_BitStream_Full() {
             bool ret_val=false;
-            if(this->size==8) ret_val=true;
+            if(this->size==7) ret_val=true;
             return ret_val;
         }
         void push_bit_right(char bit) {
-            char x = bit;
+            int b=0;
+            if(bit=='1') b=1;
+            char x = (char)(b);
             this->byte = this->byte << 1;
             this->byte |= x;
             this->size++;
